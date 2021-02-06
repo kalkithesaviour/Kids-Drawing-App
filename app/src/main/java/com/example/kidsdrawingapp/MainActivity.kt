da7ac4f0cfd7c78138bf28334e5bc9ccd9ce23b9
Vishal Adhikari <vishal.adhikari36@gmail.com>
2021-02-06 05:46:31 +0530

    saving and sharing the file functionality added

--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -8,6 +8,8 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.media.MediaScannerConnection
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -19,6 +21,9 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_brush_size.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -50,6 +55,14 @@ class MainActivity : AppCompatActivity() {
 
         ib_undo.setOnClickListener {
             drawing_view.onClickUndo()
+        }
+
+        ib_save.setOnClickListener {
+            if(isReadStorageAllowed()) {
+                BitmapAsyncTask(getBitmapFromView(fl_drawing_view_container)).execute()
+            } else {
+                requestStoragePermission()
+            }
         }
     }
 
@@ -152,6 +165,74 @@ class MainActivity : AppCompatActivity() {
         }
         view.draw(canvas)
         return returnedBitmap
+    }
+
+    private inner class BitmapAsyncTask(val mBitmap: Bitmap) : AsyncTask<Any, Void, String>() {
+
+        private lateinit var mProgressDialog: Dialog
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            showProgressDialog()
+        }
+
+        override fun doInBackground(vararg params: Any?): String {
+            var result = ""
+            if(mBitmap != null) {
+                try {
+                    val bytes = ByteArrayOutputStream()
+                    mBitmap.compress(Bitmap.CompressFormat.PNG, 90, bytes)
+                    val f = File(externalCacheDir!!.absoluteFile.toString()
+                            + File.separator + "KidsDrawingApp_"
+                            + System.currentTimeMillis() / 1000 + ".png")
+                    val fos = FileOutputStream(f)
+                    fos.write(bytes.toByteArray())
+                    fos.close()
+                    result = f.absolutePath
+                } catch (e: Exception) {
+                    result = ""
+                    e.printStackTrace()
+                }
+            }
+            return result
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            cancelProgressDialog()
+            if(result!!.isNotEmpty()) {
+                Toast.makeText(
+                        this@MainActivity,
+                        "Files is saved successfully: $result",
+                        Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                        this@MainActivity,
+                        "Something went wrong while saving the file.",
+                        Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            MediaScannerConnection.scanFile(this@MainActivity, arrayOf(result), null) {
+                path, uri -> val shareIntent = Intent()
+                shareIntent.action = Intent.ACTION_SEND
+                shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+                shareIntent.type = "image/png"
+
+                startActivity(Intent.createChooser(shareIntent, "Share"))
+            }
+        }
+
+        private fun showProgressDialog() {
+            mProgressDialog = Dialog(this@MainActivity)
+            mProgressDialog.setContentView(R.layout.dialog_custom_progress)
+            mProgressDialog.show()
+        }
+
+        private fun cancelProgressDialog() {
+            mProgressDialog.dismiss()
+        }
     }
 
     companion object {
